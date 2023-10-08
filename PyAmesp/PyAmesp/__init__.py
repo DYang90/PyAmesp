43b5302415ad8e8d369a5edf62669788d7e838a2
Chien Chao <zhaoj2333@126.com>
2023-10-08 12:23:02 +0800

    Delete PyAmesp directory

--- PyAmesp/PyAmesp/__init__.py
+++
@@ -1,4 +0,0 @@
-from PyAmesp.amesp import Amesp, GaussianExternal, read_aip, read_aop, iread_aop, write_aip
-__all__ = ['AMESP', 'Amesp', 'GaussianExternal',
-           'read_aip', 'write_aip', 'iread_aop', 'read_aop']
-AMESP = Amesp
